9cbebaa61084f0dead8f70ea0dfad7f4cc7e28e1
supereed <supereedtf2@gmail.com>
2015-03-05 13:06:52 -0500

    published

---
+++ js/loader.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+$("#main").load('/pages/band.html');
+$("#bio").click(function(){
+    $("#main").empty();
+    $("#main").load('/pages/band.html');
+    });
+$("#Anno").click(function(){
+    $("#main").empty();
+    console.log("test2");
+    $("#main").load('/pages/annotations.html');
+    });    
+});

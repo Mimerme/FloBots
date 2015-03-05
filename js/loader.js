f13e183aa8587eb4b4fd81ccc0170e8d63f47eed
supereed <supereedtf2@gmail.com>
2015-03-05 13:11:50 -0500

    fixed linking

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-$("#main").load('/pages/band.html');
+$("#main").load('pages/band.html');
 $("#bio").click(function(){
     $("#main").empty();
-    $("#main").load('/pages/band.html');
+    $("#main").load('pages/band.html');
     });
 $("#Anno").click(function(){
     $("#main").empty();
     console.log("test2");
-    $("#main").load('/pages/annotations.html');
+    $("#main").load('pages/annotations.html');
     });    
 });
